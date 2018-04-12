8687e83c95054d0f2304b976e0c9654487140dd1
Marcus Geil <mg@writereader.com>
2018-04-12 12:17:39 +0200

    Add more files to service worker for better offline support

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,6 +5,11 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/*',
+    'bower_components/gw2-coin-input/images/*',
+    'bower_components/gw2-coin-output/images/*',
+    '/src/data/collections.json',
+    '/src/data/directory.json',
+    '/src/data/metas.json',
   ],
   navigateFallback: 'index.html',
 };
