e19c7ef180c3b4c6f52c85250430edf6381b4c17
Marcus Geil <weblrmail@gmail.com>
2018-06-01 11:26:59 +0200

    Fix issues with service worker and passive touch gestures

--- src/gw2-ninja.js
+++ src/gw2-ninja.js
@@ -3,6 +3,7 @@ import {
   html
 } from "@polymer/polymer/polymer-element.js";
 import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
+import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
 import "@polymer/app-layout/app-drawer/app-drawer.js";
 import "@polymer/app-layout/app-drawer-layout/app-drawer-layout.js";
 import "@polymer/app-layout/app-header/app-header.js";
@@ -21,6 +22,14 @@ import "./page-title.js";
 import "./my-icons.js";
 import "./shared-styles.js";
 import "./online-status.js";
+
+// Gesture events like tap and track generated from touch will not be
+// preventable, allowing for better scrolling performance.
+setPassiveTouchGestures(true);
+
+// Set Polymer's root path to the same value we passed to our service worker
+// in `index.html`.
+setRootPath(MyAppGlobals.rootPath);
 
 class GW2Ninja extends PolymerElement {
   static get template() {
