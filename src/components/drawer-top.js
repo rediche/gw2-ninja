6016489a01300126af9e57ba82c69dfc7a30779e
Marcus Geil <weblrmail@gmail.com>
2018-08-15 13:25:37 +0200

    Fix space

--- src/components/drawer-top.js
+++ src/components/drawer-top.js
@@ -72,7 +72,7 @@ class DrawerTop extends connect(store)(PolymerElement) {
           <paper-icon-button icon="my-icons:close" on-tap="_closeDrawer" aria-label="Close menu"></paper-icon-button>
         </div>
         <div class="bottom">
-          Welcome <span hidden$="[[ !accountName ]]">[[ accountName ]]</span>!
+          Welcome<span hidden$="[[ !accountName ]]">&nbsp;[[ accountName ]]</span>!
         </div>
       </div>
     `;
