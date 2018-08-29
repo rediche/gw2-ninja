9e48d10cf60540249fa4f52d36ac59d5b89cf1bd
Marcus Geil <weblrmail@gmail.com>
2018-08-29 23:27:58 +0200

    Remove region-tier and add vaadin-grid to wvw-region

--- src/components/wvw/wvw-region.js
+++ src/components/wvw/wvw-region.js
@@ -1,10 +1,11 @@
 import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
-import "@polymer/polymer/lib/elements/dom-repeat.js";
 
-import "./wvw-region-tier";
+import "@vaadin/vaadin-grid/vaadin-grid";
+
+import "../utilities/gwn-progress";
 
 import { SharedStyles } from "../shared-styles.js";
-import { SharedTableStyles } from "../shared-table-styles.js";
+import { SharedWvwStyles } from "../shared-wvw-styles.js";
 
 /**
  * `wvw-region`
@@ -17,7 +18,7 @@ class WvwRegion extends PolymerElement {
   static get template() {
     return html`
     ${SharedStyles}
-    ${SharedTableStyles}
+    ${SharedWvwStyles}
     <style>
       :host {
         display: block;
@@ -35,52 +36,102 @@ class WvwRegion extends PolymerElement {
         margin-bottom: var(--spacer-small);
       }
 
-      thead th {
-        padding-bottom: 0;
+      gwn-progress {
+        font-size: .75rem;
       }
 
-      .prediction {
-        width: 2.5rem;
+      .no-text-overflow {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
       }
     </style>
 
     <h2 class="title">Europe</h2>
-    <div class="card">
-      <div class="table-scroll">
-        <table>
-          <thead>
-            <th class="prediction"></th>
-            <th>Server</th>
-            <th>Victory Points</th>
-            <th>Total Score</th>
-          </thead>
-          <tbody>
-            <template is="dom-repeat" items="{{euMatches}}">
-              <wvw-region-tier match="[[ item ]]" worlds="[[ worlds ]]"></wvw-region-tier>
-            </template>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <vaadin-grid class="card" theme="no-border row-stripes" height-by-rows items="[[euMatches]]">
+      <vaadin-grid-column width="56px" flex-grow="0">
+        <template class="header"></template>
+        <template>
+          <div><iron-icon icon$="[[_isWinningOrLoosingMatchup(item, 'green')]]"></iron-icon></div>
+          <div><iron-icon icon$="[[_isWinningOrLoosingMatchup(item, 'blue')]]"></iron-icon></div>
+          <div><iron-icon icon$="[[_isWinningOrLoosingMatchup(item, 'red')]]"></iron-icon></div>
+        </template>
+        <template class="footer"></template>
+      </vaadin-grid-column>
+
+      <vaadin-grid-column>
+        <template class="header">World</template>
+        <template>
+          <div class="no-text-overflow">[[ _generateWorldLinkNames(item.all_worlds.green, item.worlds.green, worlds) ]]</div>
+          <div class="no-text-overflow">[[ _generateWorldLinkNames(item.all_worlds.blue, item.worlds.blue, worlds) ]]</div>
+          <div class="no-text-overflow">[[ _generateWorldLinkNames(item.all_worlds.red, item.worlds.red, worlds) ]]</div>  
+        </template>
+        <template class="footer">World</template>
+      </vaadin-grid-column>
+
+      <vaadin-grid-column>
+        <template class="header">Victory Points</template>
+        <template>
+          <gwn-progress class="green" progress="[[item.victory_points.green]]" max="[[_highestVictoryPointsInMatchup(item)]]">[[item.victory_points.green]]</gwn-progress>
+          <gwn-progress class="blue" progress="[[item.victory_points.blue]]" max="[[_highestVictoryPointsInMatchup(item)]]">[[item.victory_points.blue]]</gwn-progress>
+          <gwn-progress class="red" progress="[[item.victory_points.red]]" max="[[_highestVictoryPointsInMatchup(item)]]">[[item.victory_points.red]]</gwn-progress>
+        </template>
+        <template class="footer">Victory Points</template>
+      </vaadin-grid-column>
+
+      <vaadin-grid-column>
+        <template class="header">Total Score</template>
+        <template>
+          <gwn-progress class="green" progress="[[item.scores.green]]" max="[[_highestScoreInMatchup(item)]]">[[item.scores.green]]</gwn-progress>
+          <gwn-progress class="blue" progress="[[item.scores.blue]]" max="[[_highestScoreInMatchup(item)]]">[[item.scores.blue]]</gwn-progress>
+          <gwn-progress class="red" progress="[[item.scores.red]]" max="[[_highestScoreInMatchup(item)]]">[[item.scores.red]]</gwn-progress>
+        </template>
+        <template class="footer">Total Score</template>
+      </vaadin-grid-column>
+    </vaadin-grid>
 
     <h2 class="title">North America</h2>
-    <div class="card">
-      <div class="table-scroll">
-        <table>
-          <thead>
-            <th class="prediction"></th>
-            <th>Server</th>
-            <th>Victory Points</th>
-            <th>Total Score</th>
-          </thead>
-          <tbody>
-            <template is="dom-repeat" items="{{naMatches}}">
-              <wvw-region-tier match="[[ item ]]" worlds="[[ worlds ]]"></wvw-region-tier>
-            </template>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <vaadin-grid class="card" theme="no-border row-stripes" height-by-rows items="[[naMatches]]">
+      <vaadin-grid-column width="56px" flex-grow="0">
+        <template class="header"></template>
+        <template>
+          <div><iron-icon icon$="[[_isWinningOrLoosingMatchup(item, 'green')]]"></iron-icon></div>
+          <div><iron-icon icon$="[[_isWinningOrLoosingMatchup(item, 'blue')]]"></iron-icon></div>
+          <div><iron-icon icon$="[[_isWinningOrLoosingMatchup(item, 'red')]]"></iron-icon></div>
+        </template>
+        <template class="footer"></template>
+      </vaadin-grid-column>
+
+      <vaadin-grid-column>
+        <template class="header">World</template>
+        <template>
+          <div class="no-text-overflow">[[ _generateWorldLinkNames(item.all_worlds.green, item.worlds.green, worlds) ]]</div>
+          <div class="no-text-overflow">[[ _generateWorldLinkNames(item.all_worlds.blue, item.worlds.blue, worlds) ]]</div>
+          <div class="no-text-overflow">[[ _generateWorldLinkNames(item.all_worlds.red, item.worlds.red, worlds) ]]</div>  
+        </template>
+        <template class="footer">World</template>
+      </vaadin-grid-column>
+
+      <vaadin-grid-column>
+        <template class="header">Victory Points</template>
+        <template>
+          <gwn-progress class="green" progress="[[item.victory_points.green]]" max="[[_highestVictoryPointsInMatchup(item)]]">[[item.victory_points.green]]</gwn-progress>
+          <gwn-progress class="blue" progress="[[item.victory_points.blue]]" max="[[_highestVictoryPointsInMatchup(item)]]">[[item.victory_points.blue]]</gwn-progress>
+          <gwn-progress class="red" progress="[[item.victory_points.red]]" max="[[_highestVictoryPointsInMatchup(item)]]">[[item.victory_points.red]]</gwn-progress>
+        </template>
+        <template class="footer">Victory Points</template>
+      </vaadin-grid-column>
+
+      <vaadin-grid-column>
+        <template class="header">Total Score</template>
+        <template>
+          <gwn-progress class="green" progress="[[item.scores.green]]" max="[[_highestScoreInMatchup(item)]]">[[item.scores.green]]</gwn-progress>
+          <gwn-progress class="blue" progress="[[item.scores.blue]]" max="[[_highestScoreInMatchup(item)]]">[[item.scores.blue]]</gwn-progress>
+          <gwn-progress class="red" progress="[[item.scores.red]]" max="[[_highestScoreInMatchup(item)]]">[[item.scores.red]]</gwn-progress>
+        </template>
+        <template class="footer">Total Score</template>
+      </vaadin-grid-column>
+    </vaadin-grid>
     `;
   }
 
@@ -114,6 +165,46 @@ class WvwRegion extends PolymerElement {
 
   _getTierFromID(id) {
     return id.split("-")[1];
+  }
+
+  _generateWorldLinkNames(allWorldsInLink, hostWorld, worlds) {
+    if (!allWorldsInLink || !hostWorld || !worlds) return;
+
+    const linkedWorlds = allWorldsInLink.filter(world => {
+      return world !== hostWorld;
+    });
+
+    const linkedWorldsWithNames = linkedWorlds.map(worldId => {
+      return this._resolveWorldName(worldId, worlds);
+    });
+
+    const hostWorldName = this._resolveWorldName(hostWorld, worlds);
+
+    return linkedWorlds.length > 0 ? `${hostWorldName} (${linkedWorldsWithNames.join(', ')})` : hostWorldName;
+  }
+
+  _resolveWorldName(worldId, worlds) {
+    if (!worldId || !worlds) return;
+    const foundWorld = worlds.find(world => world.id == worldId);
+    return foundWorld.name;
+  }
+
+  _isWinningOrLoosingMatchup(match, color) {
+    if (match.victory_points[color] == this._highestVictoryPointsInMatchup(match)) return "my-icons:chevron-up";
+    if (match.victory_points[color] == this._lowestVictoryPointsInMatchup(match)) return "my-icons:chevron-down";
+    return "my-icons:minus";
+  }
+
+  _highestVictoryPointsInMatchup(matchup) {
+    return Math.max(matchup.victory_points.green, matchup.victory_points.red, matchup.victory_points.blue);
+  }
+
+  _lowestVictoryPointsInMatchup(matchup) {
+    return Math.min(matchup.victory_points.green, matchup.victory_points.red, matchup.victory_points.blue);
+  }
+
+  _highestScoreInMatchup(matchup) {
+    return Math.max(matchup.scores.green, matchup.scores.red, matchup.scores.blue);
   }
 }
 
