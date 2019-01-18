706d8a3864d2750b9a4c7aa2dfb896b787a2d4e1
Marcus Geil <weblrmail@gmail.com>
2019-01-18 19:50:50 +0100

    Formatting

--- src/components/wvw/wvw-leaderboards.js
+++ src/components/wvw/wvw-leaderboards.js
@@ -34,10 +34,6 @@ class WvwLeaderboards extends PolymerElement {
     };
   }
 
-  static get observers() {
-    return ["_constructLinks(matches, worlds)"]
-  }
-
   static get template() {
     return html`
       ${SharedStyles}
@@ -74,99 +70,163 @@ class WvwLeaderboards extends PolymerElement {
         <h1 class="title">Weekly Leaderboards</h1>
         <p>Compare stats between all links on all regions.</p>
 
-        <vaadin-grid class="card" theme="no-border row-stripes" aria-label="World vs. World weekly server leaderboard" items="[[links]]" height-by-rows>
+        <vaadin-grid
+          class="card"
+          theme="no-border row-stripes"
+          aria-label="World vs. World weekly server leaderboard"
+          items="[[links]]"
+          height-by-rows
+        >
           <vaadin-grid-column>
             <template class="header">
               <vaadin-grid-sorter path="link_name">Worlds</vaadin-grid-sorter>
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.link_name ]]</div></template>
-            <template class="footer">Worlds</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.link_name ]]
+              </div></template
+            >
+            <template class="footer"
+              >Worlds</template
+            >
           </vaadin-grid-column>
 
           <vaadin-grid-column width="96px" flex-grow="0">
             <template class="header">
               <vaadin-grid-sorter path="region">Region</vaadin-grid-sorter>
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.region ]]</div></template>
-            <template class="footer">Region</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.region ]]
+              </div></template
+            >
+            <template class="footer"
+              >Region</template
+            >
           </vaadin-grid-column>
 
           <vaadin-grid-column width="68px" flex-grow="0">
             <template class="header">
               <vaadin-grid-sorter path="tier">Tier</vaadin-grid-sorter>
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.tier ]]</div></template>
-            <template class="footer">Tier</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.tier ]]
+              </div></template
+            >
+            <template class="footer"
+              >Tier</template
+            >
           </vaadin-grid-column>
 
           <vaadin-grid-column width="140px" flex-grow="0">
             <template class="header">
-              <vaadin-grid-sorter path="victory_points">Victory Points</vaadin-grid-sorter>
+              <vaadin-grid-sorter path="victory_points"
+                >Victory Points</vaadin-grid-sorter
+              >
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.victory_points ]]</div></template>
-            <template class="footer">Victory Points</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.victory_points ]]
+              </div></template
+            >
+            <template class="footer"
+              >Victory Points</template
+            >
           </vaadin-grid-column>
 
           <vaadin-grid-column width="96px" flex-grow="0">
             <template class="header">
               <vaadin-grid-sorter path="score">Score</vaadin-grid-sorter>
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.score ]]</div></template>
-            <template class="footer">Score</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.score ]]
+              </div></template
+            >
+            <template class="footer"
+              >Score</template
+            >
           </vaadin-grid-column>
 
           <vaadin-grid-column width="96px" flex-grow="0">
             <template class="header">
               <vaadin-grid-sorter path="kills">Kills</vaadin-grid-sorter>
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.kills ]]</div></template>
-            <template class="footer">Kills</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.kills ]]
+              </div></template
+            >
+            <template class="footer"
+              >Kills</template
+            >
           </vaadin-grid-column>
 
           <vaadin-grid-column width="96px" flex-grow="0">
             <template class="header">
               <vaadin-grid-sorter path="deaths">Deaths</vaadin-grid-sorter>
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.deaths ]]</div></template>
-            <template class="footer">Deaths</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.deaths ]]
+              </div></template
+            >
+            <template class="footer"
+              >Deaths</template
+            >
           </vaadin-grid-column>
 
           <vaadin-grid-column width="80px" flex-grow="0">
             <template class="header">
               <vaadin-grid-sorter path="kdr">KDR</vaadin-grid-sorter>
             </template>
-            <template><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">[[ item.kdr ]]</div></template>
-            <template class="footer">KDR</template>
+            <template
+              ><div class$="[[_addOwnWorldClass(ownWorld, item.worlds)]]">
+                [[ item.kdr ]]
+              </div></template
+            >
+            <template class="footer"
+              >KDR</template
+            >
           </vaadin-grid-column>
         </vaadin-grid>
       </div>
     `;
   }
 
+  static get observers() {
+    return ["_constructLinks(matches, worlds)"];
+  }
+
   _constructLinks(matches, worlds) {
     if (matches.length == 0 || worlds == 0) return;
-    this.set('links', matches.reduce(this._worldDataFromMatch.bind(this), []));
+    this.set("links", matches.reduce(this._worldDataFromMatch.bind(this), []));
   }
 
   _worldDataFromMatch(accumulator, match) {
     const that = this;
     const colors = ["red", "blue", "green"];
-    const region = match.id.split('-')[0] == 1 ? "NA" : "EU";
-    const tier = match.id.split('-')[1];
+    const region = match.id.split("-")[0] == 1 ? "NA" : "EU";
+    const tier = match.id.split("-")[1];
 
-    const worldData = colors.map((color) => {
+    const worldData = colors.map(color => {
       return {
         region: region,
         tier: tier,
         score: match.scores[color],
         worlds: match.all_worlds[color],
         hosting_world: match.worlds[color],
-        link_name: that._generateWorldLinkNames(match.all_worlds[color], match.worlds[color], that.worlds),
+        link_name: that._generateWorldLinkNames(
+          match.all_worlds[color],
+          match.worlds[color],
+          that.worlds
+        ),
         deaths: match.deaths[color],
         kills: match.kills[color],
         victory_points: match.victory_points[color],
         kdr: that._calcKDR(match.kills[color], match.deaths[color])
-      }
+      };
     });
 
     return accumulator.concat(worldData);
@@ -192,7 +252,9 @@ class WvwLeaderboards extends PolymerElement {
 
     const hostWorldName = this._resolveWorldName(hostWorld, worlds);
 
-    return linkedWorlds.length > 0 ? `${hostWorldName} (${linkedWorldsWithNames.join(', ')})` : hostWorldName;
+    return linkedWorlds.length > 0
+      ? `${hostWorldName} (${linkedWorldsWithNames.join(", ")})`
+      : hostWorldName;
   }
 
   /* TODO: Move to a utility file because it's used in multiple components */
