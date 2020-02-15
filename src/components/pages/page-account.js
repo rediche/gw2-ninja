b78c1320a31c3730df0b9e1eb6bfa611ea40fadb
Rediche <rediche@users.noreply.github.com>
2020-02-15 12:16:26 +0100

    Show account name on page

--- src/components/pages/page-account.js
+++ src/components/pages/page-account.js
@@ -1,5 +1,16 @@
 import {LitElement, html, css} from 'lit-element';
 
+import { connect } from "pwa-helpers/connect-mixin.js";
+
+// Load redux store
+import { store } from "../../store.js";
+
+// Lazy load reducers
+import account from "../../reducers/account.js";
+store.addReducers({
+  account
+});
+
 /**
  * `page-account` is a page that shows basic information about your GW2 account.
  *
@@ -8,10 +19,10 @@ import {LitElement, html, css} from 'lit-element';
  * @demo
  * 
  */
-class PageAccount extends LitElement {
+class PageAccount extends connect(store)(LitElement) {
     static get properties() {
         return {
-
+            name: String
         }
     }
 
@@ -22,11 +33,31 @@ class PageAccount extends LitElement {
      */
     constructor() {
         super();
+        this.name = "";
     }
 
     static get styles() {
         return [
-            css``,
+            css`
+                .container {
+                    max-width: 1100px;
+                    margin: var(--spacer-large) auto 0;
+                }
+
+                .card {
+                    padding: var(--spacer-medium);
+                    border-radius: var(--gwn-border-radius);
+                    background-color: var(--gwn-surface);
+                    color: var(--gwn-on-surface);
+                    box-shadow: var(--gwn-box-shadow);
+                    box-sizing: border-box;
+                    overflow: hidden;
+                }
+
+                .bold {
+                    font-weight: bold;
+                }
+            `,
         ];
     }
 
@@ -36,10 +67,29 @@ class PageAccount extends LitElement {
      * to render into the element.
      */
     render() {
-        return html`
-            <h1>Account</h1>
+        return html`            
+            <div class="container">
+                <div class="card">
+                    <div class="bold">Account</div>
+                    <ul>
+                        <li>${ this.name }</li>
+                    </ul>
+                </div>
+            </div>
         `;
     }
+
+    _stateChanged(state) {
+        if (!state) return;
+    
+        
+        if ( state.account ) {
+            console.log(state.account);
+            this.name = state.account.name;
+            // TODO: Redirect to frontpage if no account available
+          //this.set("hasAccount", !!state.account.name);
+        }
+      }
 
 }
 
